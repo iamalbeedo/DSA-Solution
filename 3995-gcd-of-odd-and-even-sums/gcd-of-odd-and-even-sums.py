15db12cd85fa5d8aa57de57a20951f480c4a7f33
Albeedo <154697378+iamalbeedo@users.noreply.github.com>
2026-07-15 12:17:23 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (53.03%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.py
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.py
@@ -1,3 +1,13 @@
 class Solution:
     def gcdOfOddEvenSums(self, n: int) -> int:
+        # THE MAGIC LOGIC:
+        # - Sum of first 'n' odd numbers  = n^2
+        # - Sum of first 'n' even numbers = n * (n + 1)
+        #
+        # Finding the GCD:
+        # GCD(n^2, n * (n + 1)) 
+        # = n * GCD(n, n + 1)  <-- Factor out n
+        # = n * 1               <-- Consecutive numbers always have a GCD of 1
+        # = n
+        
         return n
