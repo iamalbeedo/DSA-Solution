6b8f1d822dc0ca8d9971232d08284d6f8258d018
Albeedo <154697378+iamalbeedo@users.noreply.github.com>
2026-08-30 22:11:13 +0530

    Time: 7 ms (98.19%) | Memory: 33.6 MB (35.35%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.py
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.py
@@ -4,21 +4,16 @@ class Solution:
         if n <= 2:
             return n
 
-        # Find indices of min and max elements
         min_idx = nums.index(min(nums))
         max_idx = nums.index(max(nums))
 
-        # Determine leftmost and rightmost targets
         left = min(min_idx, max_idx)
         right = max(min_idx, max_idx)
 
-        # 1. Delete both from front
         cost_front = right + 1
 
-        # 2. Delete both from back
         cost_back = n - left
 
-        # 3. Delete one from front and one from back
         cost_both = (left + 1) + (n - right)
 
         return min(cost_front, cost_back, cost_both)
