be1d760c4d62069f2413615be622427578592982
Albeedo <154697378+iamalbeedo@users.noreply.github.com>
2026-09-03 20:39:50 +0530

    Time: 65 ms (56.10%) | Memory: 34.5 MB (98.78%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
@@ -4,18 +4,15 @@ class Solution:
         odds = [x for x in nums1 if x % 2 != 0]
         min_odd = min(odds) if odds else float('inf')
 
-        # If smallest element overall is odd, we can always make all elements odd
         if min_val % 2 != 0:
             return True
 
-        # Check if all elements can be made even
         can_be_even = True
         for num in nums1:
             if num % 2 != 0 and num <= min_odd:
                 can_be_even = False
                 break
 
-        # Check if all elements can be made odd
         can_be_odd = True
         for num in nums1:
             if num % 2 == 0 and num <= min_odd:
